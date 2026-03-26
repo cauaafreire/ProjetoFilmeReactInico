1de226425231a7846410b0d5b9a66e14e3b38280
Cauã freire <cauafreirw@gmail.com>
2026-03-26 22:28:39 +0000

    começando a esilização do projeto

--- projeto_filme/src/pages/Curiosidades/index.jsx
+++ projeto_filme/src/pages/Curiosidades/index.jsx
@@ -1,8 +1,76 @@
 function Curiosidades(){
     return(
         <div>
-            <h1>Pagina de Curiosidades</h1>
-            <p>Essa é a pagina de contatos do site.</p>
+            <h1> Curiosidades </h1>
+
+            <br />
+            <hr />
+            <br />
+
+            <h3> Ano de lançamento e direção</h3>
+            <br />
+            <p>Lançado em 2008, Batman: 
+                O Cavaleiro das Trevas foi 
+                dirigido por Christopher Nolan e faz parte de sua 
+                trilogia do Batman, se destacando por apresentar 
+                um estilo mais sério e realista, diferente dos 
+                filmes de super-herói tradicionais, o que contribuiu 
+                para o grande sucesso do filme tanto pela sua história
+                 quanto pela sua direção.
+            </p>
+
+            <br />
+            <hr />
+            <br />
+
+            <h3> Prêmios</h3>
+            <br />
+            <p>O filme venceu 2 Oscars no Academy 
+                Awards, incluindo o prêmio de Melhor Ator 
+                Coadjuvante para Heath Ledger, além de Melhor 
+                Edição de Som, e também recebeu diversas outras 
+                indicações importantes, mostrando o impacto que teve 
+                no cinema.
+            </p>
+
+            <br />
+            <hr />
+            <br />
+
+            <h3> Fatos interessantes</h3>
+            <br />
+            <p>Durante a preparação para o papel, 
+                Heath Ledger se isolou por semanas 
+                para desenvolver o Coringa, criando sua 
+                própria voz e comportamento, enquanto o filme 
+                optou por não mostrar a origem do vilão para 
+                deixá-lo mais misterioso; além disso, várias cenas 
+                foram gravadas com câmeras IMAX e efeitos práticos, 
+                como a explosão real do hospital, contribuindo para o 
+                realismo e fazendo com que o filme se tornasse um marco
+                 nos filmes de super-herói.
+            </p>
+
+            <br />
+            <hr />
+            <br />
+
+            <h3>Mistério do vilão</h3>
+            <br />
+            <p>Diferente de outras versões, 
+                o filme opta por não apresentar 
+                uma origem clara para o Coringa, 
+                permitindo que o personagem conte 
+                diferentes histórias sobre suas cicatrizes, o 
+                que aumenta sua imprevisibilidade e reforça a 
+                ideia de que ele representa o caos puro.
+            </p>
+            
+            <br />
+            <hr />
+            <br />
+            
+
         </div>
     )
 }
--- projeto_filme/src/pages/Personagens/index.jsx
+++ projeto_filme/src/pages/Personagens/index.jsx
@@ -11,21 +11,35 @@ function Personagens(){
             <img src={Bruce} alt="" />
             <br/>
             <br />
-            <h4></h4>
+            <h4>Bilionário de Bruce Wayne que vive uma vida dupla como
+                 o vigilante Batman. No filme, ele enfrenta o dilema 
+                 de continuar sendo o herói de Gotham ou permitir que 
+                 alguém como Harvey Dent assuma esse papel. Ele luta 
+                 contra o crime enquanto tenta manter sua identidade 
+                 em segredo.</h4>
             <br />
             <br />
             <h1> Coringa</h1>
             <img src={Coringa} alt="" />
             <br/>
             <br />
-            <h4></h4>
+            <h4>Vilão principal do filme, 
+                Joker é um criminoso caótico que quer 
+                provar que qualquer pessoa pode se corromper. 
+                Ele espalha o caos em Gotham e desafia diretamente 
+                os ideais de Batman, manipulando situações para mostrar
+                 o lado mais sombrio das pessoas.</h4>
             <br />
             <br />
             <h1>Alfred</h1>
             <img src={Alfred} alt="" />
             <br/>
             <br />
-            <h4></h4>
+            <h4>Mordomo fiel e conselheiro de Bruce Wayne. 
+                Alfred Pennyworth atua como uma figura paterna, 
+                oferecendo apoio emocional e orientação moral 
+                para Bruce, principalmente quando ele começa a 
+                questionar suas escolhas como Batman.</h4>
         </div>
     )
 }
